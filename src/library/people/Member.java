4e1f0aa06ab9aae0c650cc3c809e5c23c07b8cb8
Mugabepro-1 <mugabepromesse@gmail.com>
2024-10-07 20:33:59 +0200

    Added the member class

--- src/library/people/Member.java
+++ src/library/people/Member.java
@@ -1,4 +1,21 @@
 package library.people;
 
-public class Member {
+import library.materials.Book;
+
+import java.util.List;
+
+public class Member extends Person {
+    private int memberID;
+    private List<Book> borrowedBooks;
+
+    public void borrowBook(Book book){
+        borrowedBooks.add(book);
+    }
+    public void returnBook(Book book){
+        borrowedBooks.remove(book);
+    }
+    public int getBorrowedBooksCount(){
+        return borrowedBooks.size();
+    }
+
 }
