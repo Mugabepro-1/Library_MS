44942c5396280336bb867a727645273b3d9963ab
MUGABE INEZA Promesse <147703614+Mugabepro-1@users.noreply.github.com>
2025-01-27 21:15:00 +0200

    Added the toString method to person for object string representation

--- src/library/people/Person.java
+++ src/library/people/Person.java
@@ -31,4 +31,12 @@ public class Person {
     public void setEmail(String email) {
         this.email = email;
     }
+    @Override
+    public String toString(){
+        return "Person{" +
+                "fullName='" + fullName + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }        
 }
